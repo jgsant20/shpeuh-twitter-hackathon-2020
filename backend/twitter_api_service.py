41b660d82038fbfab676195030d2a73f85c37dfc
Justin Santos <jgsant20@gmail.com>
2020-10-17 23:23:11 -0500

    changes

--- backend/twitter_api_service.py
+++ backend/twitter_api_service.py
@@ -7,11 +7,19 @@ auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
 # courtesy of miss kist <3
-def get_search_result(search_query, api):
-    return tweepy.Cursor(api.search,
-                         q=search_query,
-                         count=100,
-                         result_type="popular",
-                         include_entities=True,
-                         lang="en").items()
+def get_search_result(search_query):
+  return tweepy.Cursor(api.search,
+                      q=search_query,
+                      count=100,
+                      result_type="popular",
+                      include_entities=True,
+                      lang="en").items() 
+ 
+def parse_into_json():
+  pass
 
+# def get_search_result():
+#   pass
+
+for i, status in enumerate(get_search_result("donald trump")):
+    print('{}: {}'.format(i, status.text))
