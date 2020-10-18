a9400c7963ce54c69f3be077b7fabc2f9915f752
Justin Santos <jgsant20@gmail.com>
2020-10-17 21:59:28 -0500

    cleaning up stuff

--- backend/twitter_api_service.py
+++ backend/twitter_api_service.py
@@ -6,14 +6,7 @@ auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
-
-closest_trends = api.trends_closest(10, 10)
-print(closest_trends)
-
-# courtesy of miss kist
+# courtesy of miss kist <3
 def get_search_result(search_query, api):
     return tweepy.Cursor(api.search,
                          q=search_query,
